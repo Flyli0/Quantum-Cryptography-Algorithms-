358bf35f91829ceeb0dba3e4e61a66cfb04d2988
Flyli0 <kirillrozkov653@gmail.com>
2026-04-24 20:04:28 +0500

    logic simplified

--- QuantumSystemSimulation/Qubits.py
+++ QuantumSystemSimulation/Qubits.py
@@ -1,31 +1,24 @@
 from Basis import Basis
 from State import State
+import random
 # Qubits behaviour simulation
 
 
 class Qubits:  # basis and its state = cubits
-    basis: Basis
-    state: State
-
-    def __init__(self, basis: Basis, state: State):  # cubit initialization
+    def __init__(self, basis: Basis, state: int):  # cubit initialization
         self.basis = basis
-        if self.basis in [Basis.DIAGONAL] and state in [State.DEGREE45, State.DEGREE135]:
-            self.state = state
-        elif self.basis in [Basis.RECTILINEAR] and state in [State.DEGREE0, State.DEGREE90]:
+        if state not in [0,1]:
+            raise AttributeError
+        else:
             self.state = state
 
-    def measure_basis(self, try_basis: Basis):  # basis measurement simulation
+    def measure(self, try_basis: Basis) -> int:  # basis measurement simulation
         if self.basis == try_basis:
-            return True
+            return self.state
         else:  # if measurement is wrong Qubit collapses
             self.basis = Basis.WHATISTHAT
-            self.state = State.DEGREEWTF
+            self.state = random.randint(0,1)
+            return self.state
 
-    def measure_state(self, try_state: State):  # state measurement simulation
-        if self.state == State:
-            return True
-        else:  # if measurement is wrong Qbit collapses
-            self.state = State.DEGREEWTF
-            self.basis = Basis.WHATISTHAT
 
 
--- QuantumSystemSimulation/State.py
+++ QuantumSystemSimulation/State.py
@@ -2,8 +2,6 @@ from enum import Enum
 
 
 class State(Enum):
-    DEGREE0 = 0
-    DEGREE90 = 1
-    DEGREE45 = 0
-    DEGREE135 = 1
-    DEGREEWTF = "UNDEFINED"
+    BIT0 = 0
+    BIT1 = 1
+
